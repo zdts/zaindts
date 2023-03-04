6de69c008a80a61836133aaa2456ec953c86bea3
Rashid Mehmood_PIAIC53596 <55920727+zdts@users.noreply.github.com>
2023-03-04 23:48:02 +0500

    Update index.js
    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+           Zain Digital Technologies is a Brand Manufacturing All around the world save your mony and Time ⏱️
           </Link>
         </div>
       </div>
