fc4a138a5ed1ace8d4bb912c44c18aeddedbb8d5
Rashid Mehmood_PIAIC53596 <55920727+zdts@users.noreply.github.com>
2023-03-04 23:53:42 +0500

    Update index.js
    2nd Changes

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Graphic Design',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
@@ -14,7 +14,7 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Web & App Development',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
@@ -24,7 +24,7 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Digital Media Marketing And Lead Genration',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
